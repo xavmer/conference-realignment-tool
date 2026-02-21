d2da1b6a522c93762429936a28f869abdaff00e2
xavmer <xavier@lefflers.com>
2026-02-21 17:06:38 -0600

    github add

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+    root: __dirname, // <-- FIX
+  },
 };
 
 export default nextConfig;
